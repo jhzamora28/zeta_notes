c5003ba6985463705deba289db3667023a6f54cf
Andy Xiang <andy@ttyn.me>
2017-11-22 00:06:21 -0800

    [dm] start a simple dm

--- app/src/main/java/csueb/zeta/zwriter/zwriter2017f/dataManager.java
+++ app/src/main/java/csueb/zeta/zwriter/zwriter2017f/dataManager.java
@@ -1,8 +1,27 @@
 package csueb.zeta.zwriter.zwriter2017f;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 /**
  * Created by arbeitandy on 11/21/17.
  */
 
 public class dataManager {
+
+    public File[] getFileList(String folderName) {
+        File folder = new File(folderName);
+        File[] fileList = folder.listFiles();
+        return fileList;
+    }
 }
+/**
+ * Usage:
+  dm = new dataManager();
+
+  String home = Environment.getExternalStorageDirectory().toString() + "/zwriter/" + folderName;
+  File[] fileList = dm.getFileList(home);
+  **/
