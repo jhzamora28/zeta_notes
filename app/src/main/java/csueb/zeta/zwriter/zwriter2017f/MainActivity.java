17019275071b155196484e65b2abadfdcbffbd07
Andy Xiang <andy@ttyn.me>
2017-11-24 09:36:44 -0800

    [mainActivity] add youtube link (to a random video) note: since I already changed mainActivity code, I just get the tutorial link work so that we get some more features to demo also updated the tutorial code to make it cleaner - and now we can move them to a separate class if required. #TODO will make the file list work later today. maybe i can also make the folder list work since they looks the same

--- app/src/main/java/csueb/zeta/zwriter/zwriter2017f/MainActivity.java
+++ app/src/main/java/csueb/zeta/zwriter/zwriter2017f/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 import android.content.Intent;
+import android.net.Uri;
+import android.content.ActivityNotFoundException;
 
 import com.example.wenjin.zwriter.R;
 
@@ -44,6 +46,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         folder2 = (Button) findViewById(R.id.folder2);
         folder1 = (Button) findViewById(R.id.folder1);
 
+        final String tutorialYoutubeId = "twZggnNbFqo";
+        final String [] emailArray = new String[] {"zetagroup@csueb.edu"};
+        final String emailSubject = new String("From Zwriter user");
+        final String emailText = new String("I am using the Zwriter and ...");
+        final String chooseClient = new String ("Please Choose a client: ");
+
+
         setting.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick (View view) {
@@ -57,7 +66,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                 backgroundUtil.changeBackgroundColor(item, bg);
                                 break;
                             case R.id.contactinfo:
-                                sendEmail();
+                                sendEmail(emailArray, emailSubject, emailText, chooseClient);
+                                break;
+                            case R.id.tutorial:
+                                youtubeTutorial(tutorialYoutubeId);
                                 break;
                             default:
                                 break;
@@ -158,15 +170,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-    public void sendEmail() {
+    public void sendEmail(String [] emailArray,
+                          String subject,
+                          String text,
+                          String chooseClient) {
         Intent e = new Intent(Intent.ACTION_SEND);
         e.setType("message/rfc822");
-        e.putExtra(Intent.EXTRA_EMAIL, new String[] {"zetagroup@csueb.edu"} );
-        e.putExtra(Intent.EXTRA_SUBJECT, "email from zwriter");
-        e.putExtra(Intent.EXTRA_TEXT, "Test");
+        e.putExtra(Intent.EXTRA_EMAIL, emailArray );
+        e.putExtra(Intent.EXTRA_SUBJECT, subject);
+        e.putExtra(Intent.EXTRA_TEXT, text);
 
-        startActivity(Intent.createChooser(e, "Please Choose a client: "));
+        startActivity(Intent.createChooser(e, chooseClient ));
 
+    }
+
+    public void youtubeTutorial(String id){
+        Intent appIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + id));
+        Intent webIntent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://www.youtube.com/watch?v=" + id));
+        try {
+            startActivity(appIntent);
+        } catch (ActivityNotFoundException ex) {
+            startActivity(webIntent);
+        }
     }
 
 }
